a2d1f78f5ed03209c5c240aef833cdca55a76494
jake <jakerasmusson@gmail.com>
2024-09-08 11:33:58 -0500

    grapple is done

--- jam2024/Assets/Scripts/Grappling.cs
+++ jam2024/Assets/Scripts/Grappling.cs
@@ -6,6 +6,7 @@ public class Grappling : MonoBehaviour
 {
     [SerializeField] private float grappleLength;
     [SerializeField] private LayerMask grappleLayer;
+    [SerializeField] private LineRenderer rope;
 
     private Vector3 grapplePoint;
     private DistanceJoint2D joint;
@@ -15,6 +16,7 @@ public class Grappling : MonoBehaviour
     {
         joint = gameObject.GetComponent<DistanceJoint2D>();
         joint.enabled = false;
+        rope.enabled = false;
         
     }
 
@@ -34,11 +36,19 @@ public class Grappling : MonoBehaviour
                     joint.connectedAnchor = grapplePoint;
                     joint.enabled = true;
                     joint.distance = grappleLength;
+                    rope.SetPosition(0, grapplePoint);
+                    rope.SetPosition(1, transform.position);
+                    rope.enabled = true;
                 }
         }
 
         if (Input.GetMouseButtonUp(0)) {
             joint.enabled = false;
+            rope.enabled = false;
+        }
+
+        if (rope.enabled == true) {
+            rope.SetPosition(1, transform.position);
         }
         
     }
