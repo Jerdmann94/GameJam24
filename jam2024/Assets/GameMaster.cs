c075e1e33e35eee4f87ea0f20453a99eaac6bd08
jerdmann94 <erdmannjacob0@gmail.com>
2024-09-08 09:40:20 -0500

    f

---
+++ jam2024/Assets/GameMaster.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public static bool GameRunning = false;
+    void Start()
+    {
+        GameRunning = false;
+    }
+
+    public static void RestartGame()
+    {
+        
+    }
+}
--- jam2024/Assets/Scripts/PlayerScript.cs
+++ jam2024/Assets/Scripts/PlayerScript.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Camera;
 
-public class test : MonoBehaviour
+public class PlayerScript : MonoBehaviour
 {
+    private Camera _camera;
+
     // Start is called before the first frame update
     void Start()
     {
-        print("jake sucks");
+        _camera = main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_camera) _camera.transform.LookAt(transform.position + new Vector3(0, 1, 0));
     }
 }
