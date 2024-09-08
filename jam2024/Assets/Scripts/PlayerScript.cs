416ccb13fc2604aa18334688d471e44ad0caa192
ktunebe <ktunebe@gmail.com>
2024-09-08 11:18:05 -0500

    movement stuffz

--- jam2024/Assets/Scripts/PlayerScript.cs
+++ jam2024/Assets/Scripts/PlayerScript.cs
@@ -7,30 +7,56 @@ public class PlayerScript : MonoBehaviour
 {
     private Camera _camera;
 
+    private Transform player;
+
+    private Rigidbody2D rb;
+
+    private bool isGrounded;
+
+    public float runMovement = 2f;
+
+    public float jumpForce = 4f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         _camera = main;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_camera) _camera.transform.LookAt(transform.position + new Vector3(0, 1, 0));
+        if (_camera) _camera.transform.position = transform.position + new Vector3(0, 0, -10);
 
-        if (Input.GetKeyDown("a"))
+        if (Input.GetKey("a"))
         {
-            transform.position += new Vector3(-0.1f,0, 0);
+            transform.position += new Vector3(-runMovement, 0, 0) * Time.deltaTime;
         }
 
-        if (Input.GetKeyDown("d"))
+        if (Input.GetKey("d"))
         {
-            transform.position += new Vector3(0.1f, 0, 0);
+            transform.position += new Vector3(runMovement, 0, 0) * Time.deltaTime;
         }
 
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown("space") && isGrounded)
         {
-            //JUMP
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
     }
 }
